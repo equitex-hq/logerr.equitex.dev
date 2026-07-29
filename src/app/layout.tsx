5c6b2d74946c0adc7956b3bdfc7b80ac50d5ede3
Erik Štvrtecký <estvrtecky@gmail.com>
2026-07-29 10:58:09 +0200

    LOG-7: Remove old fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { instrumentSerif, inter, robotoMono, satoshi } from "@/app/fonts/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +17,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+      className={`${satoshi.variable} ${instrumentSerif.variable} ${inter.variable} ${robotoMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
