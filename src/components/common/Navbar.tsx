806611756f9185a0d59f6512571850160f747761
Erik Štvrtecký <estvrtecky@gmail.com>
2026-07-30 16:18:24 +0200

    LOG-3: Add responsive mobile menu to Navbar component

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { FaBars, FaXmark } from "react-icons/fa6";
+
+import Button from "@/components/ui/Button";
 
 export default function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <header className="sticky z-(--z-sticky) top-0">
       <nav className="border-b border-(--border) bg-(--bg)">
@@ -29,7 +41,7 @@ export default function Navbar() {
                 LogErr
               </Link>
             </div>
-            <ul className="flex gap-6">
+            <ul className="hidden lg:flex gap-6">
               <li>
                 <Link
                   href="/docs"
@@ -46,7 +58,7 @@ export default function Navbar() {
               </li>
             </ul>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="hidden lg:flex items-center gap-4">
             <Link
               href="/dashboard"
               className="px-2 py-1 border border-(--border) rounded-md text-xs bg-(--bg-light) transition-shadow duration-300 hover:shadow">
@@ -54,6 +66,74 @@ export default function Navbar() {
             </Link>
             <div className="w-8 h-8 border border-(--border) rounded-full bg-(--bg-light)"></div>
           </div>
+          <Button
+            onClick={toggleMenu}
+            aria-label=""
+            variant="default"
+            layout="icon-only"
+            className="lg:hidden -mr-2 border-none bg-transparent">
+            <FaBars aria-hidden="true" />
+          </Button>
+
+          {isMenuOpen && (
+            <div className="fixed inset-0 lg:hidden flex flex-col overflow-hidden bg-(--bg)">
+              <div className="flex items-center justify-between gap-16 p-4 md:px-8 border-b border-(--border)">
+                <div className="flex items-center space-x-3">
+                  <Link
+                    href="https://www.equitex.dev"
+                    target="_blank"
+                    className="relative flex w-8 h-8">
+                    <Image
+                      src="/logo-equitex.svg"
+                      alt="Equitex Logo"
+                      fill
+                      priority
+                      className="dark:invert"
+                    />
+                  </Link>
+                  <div
+                    aria-hidden="true"
+                    className="w-px h-8 bg-(--border)"></div>
+                  <Link
+                    href="/"
+                    className="font-heading font-medium text-2xl leading-none">
+                    LogErr
+                  </Link>
+                </div>
+                <Button
+                  onClick={toggleMenu}
+                  layout="icon-only"
+                  className="-mr-2 border-none bg-transparent">
+                  <FaXmark />
+                </Button>
+              </div>
+              <div className="grow overflow-y-auto">
+                <ul className="flex flex-col gap-2 p-4">
+                  <li>
+                    <Link
+                      href="/docs"
+                      className="block px-4 py-2 rounded-lg hover:bg-btn-hover transition-colors">
+                      Docs
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/pricing"
+                      className="block px-4 py-2 rounded-lg hover:bg-btn-hover transition-colors">
+                      Pricing
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="p-4 border-t border-(--border)">
+                <Link
+                  href="/dashboard"
+                  className="block px-4 py-2 rounded-lg text-btn text-center bg-fg hover:bg-(--neutral-800) dark:hover:bg-(--neutral-200) transition duration-300">
+                  Dashboard
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       </nav>
     </header>
